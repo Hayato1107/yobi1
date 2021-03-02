96dcdcdc357bca18c7b6e12e7f287482c865e203
EC2 Default User <ec2-user@ip-172-31-28-38.us-east-2.compute.internal>
2021-03-02 04:34:46 +0000

    M

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//獲得用
+Route::get('app1', function () {
+    return view('yt.check'); //提出用
+    // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
+});
 
-Route::get('/', function () {
-    return view('welcome');
+//確認用
+Route::get('cccheck', function () {
+    return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
 });
