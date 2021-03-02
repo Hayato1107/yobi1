eccd3a9a792072a3269dd30e778692eb40f81a92
EC2 Default User <ec2-user@ip-172-31-28-38.us-east-2.compute.internal>
2021-03-02 17:50:29 +0000

    M

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 //獲得用
 Route::get('app1', function () {
-    // return view('yt.check'); //提出用
-    return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
+    return view('yt.check'); //提出用
+    // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
 });
 
 //確認用
