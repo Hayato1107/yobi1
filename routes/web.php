a4ddde424d0124dd82ff4867de8baed7e61ee6b7
EC2 Default User <ec2-user@ip-172-31-28-38.us-east-2.compute.internal>
2021-03-06 02:03:12 +0000

    m

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,17 @@
 //獲得用
 Route::get('app1', function () {
     return view('yt.check'); //提出用
-    // return view('yt.job'); //チャットレディー
+    // return view('yt.pp'); //パパ活LINE@
     
+    // return view('yt.job'); //チャットレディー
     // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
 });
 
 //確認用
 Route::get('cccheck', function () {
-    return view('yt.job'); //チャットレディー
+    return view('yt.pp'); //パパ活LINE@
     
+    // return view('yt.job'); //チャットレディー
     // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
 });
 
