6fd4608f85400192a340f75dfcda742d1a305fa7
EC2 Default User <ec2-user@ip-172-31-28-38.us-east-2.compute.internal>
2021-04-01 03:20:06 +0000

    M

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -11,8 +11,8 @@ class AdController extends Controller
         $user_agent =  $request->header('User-Agent');
         if ((strpos($user_agent, 'iPhone') !== false)) {
             // return redirect('https://a-trade.jp/redirect/popo?media=G16531'); //PoPo - Aトレード
-            // return redirect('https://a-trade.jp/redirect/moa?media=G16531'); //モア - Aトレード
-            return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
+            return redirect('https://a-trade.jp/redirect/moa?media=G16531'); //モア - Aトレード
+            // return redirect('https://trading-ad.net/lp/livede55/lady01/?pf=atrade&b=G16531'); //ライブでゴーゴー - Aトレード
             // return redirect('https://a-trade.jp/redirect/berry?media=G16531'); //ベリー - Aトレード
             // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //ワクワクP活iOS - fam
         }
